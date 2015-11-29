cc8a258eaaf6515d0abc4a9ff7881f83ea0fe5be
Jon Miller <jon4988@gmail.com>
2015-11-29 11:39:27 -0600

    Create directories inside ~/.vim, install Vundle

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
+echo "creating ~/.vim/ directory"
+mkdir ~/.vim
+
+echo "copying bundle/ to ~/.vim/bundle/"
+cp -r bundle/ ~/.vim/bundle/
+
+echo "copying colors/ to ~/.vim/colors/"
+cp -r colors/ ~/.vim/colors/
+
 echo "copying .vimrc to correct location"
 cp .vimrc ~/
+
+echo "installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "installing vim plugins via Vundle"
+vim +PluginInstall +qall
+
+echo "complete!"
